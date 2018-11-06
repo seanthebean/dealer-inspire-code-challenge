e9b78de8955072649aa5b41c70f292a86d0f5bc4
Sean Knutson <seandknutson@gmail.com>
2018-11-05 23:30:18 -0500

    store message in database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -11,12 +12,24 @@ class HomeController extends Controller
     }
 
     public function contact(Request $request) {
-        $request->validate([
+        // validate request
+        $validatedData = $request->validate([
             'full_name' => 'required|max:100',
             'email' => 'required|email|max:100',
             'phone' => 'nullable|phone|max:50', // phone rule is defined in AppServiceProvider.php
             'message' => 'required',
         ]);
+
+        // email guy smiley
+
+        // store in DB
+        DB::table('messages')
+            ->insert([
+                'full_name' => $validatedData['full_name'],
+                'email' => $validatedData['email'],
+                'phone' => $validatedData['phone'],
+                'message' => $validatedData['message'],
+            ]);
 
         return response()->json([
             'success' => true,
---
+++ database/migrations/2018_11_06_041639_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('full_name', 100);
+            $table->string('email', 100);
+            $table->string('phone', 50)->nullable();
+            $table->string('message');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
