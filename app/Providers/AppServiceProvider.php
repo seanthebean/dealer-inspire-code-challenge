1b83f5305d33dd7bc7762a04c159bb1f5841f257
Sean Knutson <seandknutson@gmail.com>
2018-11-07 20:42:49 -0500

    minor formatting stuff

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,11 +16,11 @@ class AppServiceProvider extends ServiceProvider
     {
         // phone number validation
         // credit: https://stackoverflow.com/a/39331613/814160
-        Validator::extend('phone', function($attribute, $value, $parameters, $validator) {
+        Validator::extend('phone', function ($attribute, $value, $parameters, $validator) {
             return preg_match('%^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$%i', $value) && strlen($value) >= 10;
         });
 
-        Validator::replacer('phone', function($message, $attribute, $rule, $parameters) {
+        Validator::replacer('phone', function ($message, $attribute, $rule, $parameters) {
             return str_replace(':attribute', $attribute, 'The :attribute must be a valid phone number.');
         });
     }
